7a5d8c38bcf60d5c785cddb15d101cd9cea8fa75
vyacheslav-ivanov <ivv.ulan-ude.1996@mail.ru>
2017-01-17 14:31:03 +0300

    Add files via upload

---
+++ Ivanov_professor_class.py
@@ -0,0 +1,28 @@
+import re
+
+
+class Professor:
+    def __init__(self, fullname, workplace, interest, telnum):
+        self.fullname = fullname
+        self.workplace = workplace
+        self.interest = interest
+        self.telnum = telnum
+
+file = open('Professors.txt', 'r', encoding='utf-8')
+text = file.read()
+text = text.split('<div class="main content small">')
+
+arr = []
+for i in text:
+    fullname = re.findall('title="(.+?)"', i, flags=re.DOTALL)
+    workplace = re.findall('<a class="link" href=.+>(.+)</a>', i)
+    interest = re.findall('<a class="tag".+?>(.+?)</a>', i, flags=re.DOTALL)
+    telnum = re.findall('<span>(\+7.+?)</span>', i, flags=re.DOTALL)
+    arr.append(Professor(fullname, workplace, interest, telnum))
+
+for j in arr:
+    print('ФИО:', ' '.join(j.fullname))
+    print('Место работы:', ', '.join(j.workplace))
+    print('Интересы:', ', '.join(j.interest))
+    print('Номер телефона:', ' '.join(j.telnum))
+    print()
